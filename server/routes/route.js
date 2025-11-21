30e7b3a03bb97345ab2167f856d9bb3214c23da2
Alfikri <alfzein19@gmail.com>
2025-11-21 18:32:01 +0700

    feat: added protection middleware and a protected route

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,6 @@
 const { UserController } = require('../controllers/userController');
 const { AuthController } = require('../controllers/authController');
+const protect = require('../middlewares/protection');
 const express = require('express');
 const route = express.Router();
 
@@ -12,5 +13,10 @@ route.post('/register', AuthController.register);
 route.post('/login', AuthController.login);
 route.post('/login-otp/request', AuthController.requestLoginOtp);
 route.post('/login-otp/verify', AuthController.verifyLoginOtp);
+
+//Protected route
+route.get('/me', protect, (req, res) => {
+    res.json({ message: `Hello, your user ID is ${req.user.id} and email is ${req.user.email}` });
+});
 
 module.exports = route;
